43a85077124a0db63e9bbc7e03e1d7e38fb9ad3a
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-03-09 15:36:22 -0800

    components made

---
+++ survey-react/src/App.js
@@ -0,0 +1,12 @@
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+    <Footer />
+    <h1>Hello from App</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ survey-react/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import {useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
+
+const Footer = () =>{
+  return(
+    <>
+    <h1>Footer Component</h1>
+    </>
+  )
+}
+
+
+export default Footer
